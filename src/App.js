963cd66e2d71e68c3f77b8be6347a4ccc5a32249
unknown <nhi3373@gmail.com>
2023-11-15 22:02:08 +0900

    css변경

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./components/views/MainPage/Main";
+import Choicecloud from "./components/views/SetcloudPage/Choicecloud";
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Main />} />
+          <Route exact path="/choicecloud" element={<Choicecloud />} />
         </Routes>
       </BrowserRouter>
     </div>

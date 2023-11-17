0e8ed3b27637f9f56550ec2aed5b37a3307b5269
unknown <nhi3373@gmail.com>
2023-11-17 14:14:59 +0900

    구름꾸미기 퍼블리싱 1차완성

--- src/components/views/SetcloudPage/Designcloud.js
+++ src/components/views/SetcloudPage/Designcloud.js
@@ -5,16 +5,20 @@ export default function Designcloud() {
     return (
         <div className='design-container'>
             <div className='title-container'>
-                <img src='/img/designcloud/arrow.png' alt='arrowbtn'></img>
-                <div className='design-title'>구름 꾸미기</div>
+                <div className='design-title'>
+                    <img src='/img/designcloud/arrow.png' alt='arrowbtn'></img>
+                    구름 꾸미기</div>
             </div>
-            <div className='customed-cloud'><img src='' alt=''></img></div>
+            <div className='customed-cloud'><img src='/img/designcloud/default_cloud.png' alt='기본구름'></img></div>
             <div className='design-board'>
                 <div className='custom-color'>
                     <div className='color-text'>구름 색상 선택</div>
                     <div className='colorpalette-container'>
-                        <div className='yellow-palette'></div>
-                        <div className='pink-palette'></div>
+                        <div className='palette-detail'><img src='/img/designcloud/pink_square.png' alt='default_face'></img></div>
+                        <div className='palette-detail'><img src='/img/designcloud/yellow_square.png' alt='sadface'></img></div>
+                        <div className='palette-detail'><img src='/img/designcloud/purple.png' alt='sadface'></img></div>
+                        <div className='palette-detail'><img src='/img/designcloud/orange.png' alt='sadface'></img></div>
+                        <div className='palette-detail'><img src='/img/designcloud/mint.png' alt='sadface'></img></div>
                     </div>
 
                 </div>
@@ -27,11 +31,16 @@ export default function Designcloud() {
                         <div className='palette-detail'><img src='/img/designcloud/wink_face.png' alt='sadface'></img></div>
                         <div className='palette-detail'><img src='/img/designcloud/surprised_face.png' alt='sadface'></img></div>
                     </div>
-
                 </div>
                 <div className='custom-icon'>
                     <div className='icon-text'>아이콘 선택</div>
-                    <div className='iconpalette-container'><div className='icon-palette'></div></div>
+                    <div className='iconpalette-container'>
+                        <div className='palette-detail'><img src='/img/designcloud/heart.png' alt='default_face'></img></div>
+                        <div className='palette-detail'><img src='/img/designcloud/star.png' alt='sadface'></img></div>
+                        <div className='palette-detail'><img src='/img/designcloud/moon.png' alt='sadface'></img></div>
+                        <div className='palette-detail'><img src='/img/designcloud/sun.png' alt='sadface'></img></div>
+                        <div className='palette-detail'><img src='/img/designcloud/rain.png' alt='sadface'></img></div>
+                    </div>
                 </div>
             </div>
             <button className='custom-complete'>구름 완성</button>

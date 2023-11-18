c9b2772f7cb70345616558f12c82b501263a719c
imddoy <imddoy@duksung.ac.kr>
2023-11-18 23:07:35 +0900

    Feat: 로그인회원가입 구현

--- src/components/views/LoginPage/LoginBtnStyle.js
+++ src/components/views/LoginPage/LoginBtnStyle.js
@@ -4,13 +4,17 @@ export const LoginWrapper = styled.div`
   display: flex;
   flex-direction: column;
   jsutify-content: center;
+  align-items: center;
+  margin-top: 10px;
+  gap: 10px;
 `;
 export const ID = styled.input`
   width: 280px;
   height: 42px;
   border-radius: 5px;
+  border: none;
   background: rgba(255, 255, 255, 0.85);
-  &: placeholder {
+  &:placeholder {
     color: #606060;
     font-family: Inter;
     font-size: 18px;
@@ -19,13 +23,19 @@ export const ID = styled.input`
     line-height: 18px; /* 100% */
     letter-spacing: -0.165px;
   }
+  &:focus {
+    outline: none;
+  }
+  padding: 0 10px;
+  box-sizing: border-box;
 `;
 export const PW = styled.input`
   width: 280px;
   height: 42px;
   width: 280px;
   height: 42px;
-  &: placeholder {
+  border: none;
+  &:placeholder {
     color: #606060;
     font-family: Inter;
     font-size: 18px;
@@ -34,4 +44,39 @@ export const PW = styled.input`
     line-height: 18px; /* 100% */
     letter-spacing: -0.165px;
   }
+  &:focus {
+    outline: none;
+  }
+  padding: 0 10px;
+  box-sizing: border-box;
+`;
+export const Submit = styled.div`
+  border-radius: 50px;
+  background: #448af7;
+  width: 283px;
+  height: 45px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  text-align: center;
+  font-family: Anek Devanagari;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+export const Info = styled.div`
+  color: #303030;
+  text-align: center;
+  font-family: Inter;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 18px; /* 120% */
+  letter-spacing: -0.165px;
+`;
+export const Signup = styled.a`
+  color: #e7544b;
+  text-decoration: none;
 `;
